30a7c6e81f8721e0002be2df0206b6e0dd1a80b4
Ezekiel Cochran <zeke.j.cochran@gmail.com>
2022-10-19 13:53:35 -0500

    brute force checks for maximum distance

--- brute_force.py
+++ brute_force.py
@@ -1,5 +1,23 @@
 import math
 
+def max_name_length(list):
+    max = len(list[0][2])
+    for i in range(2, len(list)):
+        this_length = len(list[i][2])
+        if this_length > max:
+            max = this_length
+    return max
+
+def pad (list):
+    length = max_name_length(list)
+    print("LENGTH: {}".format(length))
+    for town in list:
+        name = town[2]
+        while len(name) != length:
+            name = name + "-"
+        # print(name + "|")
+        town[2] = name
+
 def village_string_to_list(input_line):
     line_list = input_line.split()
     line_list[0] = int(line_list[0])
@@ -9,11 +27,13 @@ def village_string_to_list(input_line):
 def distance_between(a, b):
     x_difference = b[0] - a[0]
     y_difference = b[1] - a[1]
-    return math.sqrt(x_difference * x_difference + y_difference * y_difference)
+    # return math.sqrt(x_difference * x_difference + y_difference * y_difference)
+    return x_difference * x_difference + y_difference * y_difference
 
 def input_villages():
-    # result = [[10, 39, 'mekelle'], [15, 21, 'wukro'], [20, 44, 'adigrat'], [40, 10, 'axum'], [56, 43, 'debre_damo']]
-    result = [[0, 0, 'A'], [3, 3, 'B'], [6, 7, 'C'], [8, 0, 'D'], [11, 6, 'E'], [0, 7, 'F']]
+    result = [[10, 39, 'mekelle'], [15, 21, 'wukro'], [20, 44, 'adigrat'], [40, 10, 'axum'], [56, 43, 'debre_damo']]
+    # result = [[0, 0, 'A'], [3, 3, 'B'], [6, 7, 'C'], [8, 0, 'D'], [11, 6, 'E'], [0, 7, 'F']]
+    # result = [0, 0, 'A'], [1000, 500, 'B']
     return result
 
 def brute_force(village_list):
@@ -27,7 +47,7 @@ def brute_force(village_list):
             if (this_distance < min):
                 min = this_distance
                 result = [first[2], second[2]]
-    if (min == 1200):
+    if (min == MAXIMUM_DISTANCE):
         return None
     result.append(min)
     return result
@@ -35,4 +55,12 @@ def brute_force(village_list):
 
 MAXIMUM_DISTANCE = 1200
 villages = input_villages()
-print(brute_force(villages))
+pad(villages)
+solution = brute_force(villages)
+if solution == None:
+    output = "There are no villages within {max_distance} miles of each other.".format(max_distance = MAXIMUM_DISTANCE)
+else:
+    solution[2] = math.sqrt(solution[2])
+    output = "The closest villages are {0[0]} and {0[1]}, which are {0[2]:.2f} miles apart.".format(solution)
+print("\n"*2)
+print(output)
