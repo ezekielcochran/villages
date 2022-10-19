edcfe139185bf6ecd99368b6e0c029ab853a088d
Ezekiel Cochran <zeke.j.cochran@gmail.com>
2022-10-19 13:56:04 -0500

    Added comments to try and protect input message in main branch

--- cochran03.py
+++ cochran03.py
@@ -11,13 +11,13 @@ def distance_between(a, b):
     y_difference = b[1] - a[1]
     return math.sqrt(x_difference * x_difference + y_difference * y_difference)
 
-def input_villages():
+def input_villages(): # This is the part of the main branch I would like to keep the same
     print("Enter the total number of villages: ", end = "")
     village_count = int(input())
     result = []
     for i in range(village_count):
         line = input()
         result.append(village_string_to_list(line))
-    return result
+    return result # To here
 
 villages = input_villages()
